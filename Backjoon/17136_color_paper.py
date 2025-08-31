7f1f6fd9d221fb63aa24b6c928e2f39d83b35bd9
Lee-JunH <harry.lee1006@gmail.com>
2025-09-01 00:44:56 +0900

    solve paper

--- Backjoon/17136_color_paper.py
+++ Backjoon/17136_color_paper.py
@@ -21,19 +21,68 @@ backjoon_17136 - 색종이 붙이기 - G2
     - 짝수인 경우는 예외로 6인 경우 3x3 4개, 8 - 4개, 10 - 4개
 """
 
-def check_paper(size):
+def find_one(r, c):     # r,c 다음 칸들 중 1인 인덱스 찾기
+    temp = r
+    for i in range(r, N):
+        if i > temp:
+            c = 0
+        for j in range(c, N):
+            if paper[i][j] == 1:
+                return i, j
+    return 10, 0 # 없으면 마지막으로
     
+
+def square(r, c, size): # size로 덮이는지 확인
+    hap = 0
+    if r+size > 10 or c+size > 10:
+        return 0
+    for i in range(r, r+size):
+        for j in range(c, c+size):
+            hap += paper[i][j]
+    if hap == size**2:
+        return 1
+    return 0
+
+def draw_paper(r, c, size, num):    # 색칠하기
+    for i in range(r, r+size):
+        for j in range(c, c+size):
+            paper[i][j] = num
+
+def backtrack(r, c):
+    global min_cnt
+
+    r, c = find_one(r, c)   # 1인 지점 찾아서 시작하기
+
+    if r == 10 and c == 0:   # 마지막까지 봤을 때 개수 확인
+        if sum(color) < min_cnt:   # 개수가 min값이면 업데이트
+            min_cnt = sum(color)
+        return
+    
+    for i in range(5, 0, -1):   # 색종이 채우기
+        if color[i] == 5:   # 색종이가 이미 5장이면 다음 색종이
+            continue
+
+        if square(r, c, i): # 채울 수 있는지 보고
+            color[i] += 1   # 덮이는 색 +1
+            draw_paper(r, c, i, 0)  # 0으로 덮기
+
+            backtrack(r, c)
+            
+            # 돌아와서 다른 색종이로 채워보기 위해 다시 채우기
+            draw_paper(r, c, i, 1)
+            color[i] -= 1
+            # 다음 크기의 색종이로 채우기
 
 N = 10
 paper = [list(map(int, input().split())) for _ in range(N)]
 
-p1 = 0
-p2 = 0
-p3 = 0
-p4 = 0
-p5 = 0
+# 각 종이 개수 변수
+color = [0 for _ in range(6)]   # 사용한 색종이 리스트
 
-for p in range(10, 0, -1):
-    for i in range(0, 10-p +1):
-        for j in range(0, 10-p +1):
-            check_paper(p)
+min_cnt = 26
+backtrack(0, 0)    # 시작하는 위치(0,0), 색종이 사용 횟수
+
+if min_cnt == 26:
+    print(-1)
+else:
+    print(min_cnt)
