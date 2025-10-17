6aadba91c538f08ac758355c0fc2fad154ea1996
junh <harry.lee1006@gmail.com>
2025-10-17 23:17:07 +0900

    공항

--- Backjoon/10775_airport.py
+++ Backjoon/10775_airport.py
@@ -9,28 +9,45 @@ Backjoon_10775 - 공항 - G2
 
 풀이
 - 1~g 중 어디에 놓느냐에 따라 개수가 달라진다.
-- 확인해야 하는 경우의 수가 g * g * ... 10^5 반복
-- 다른 규칙 찾기
-- 1 2 3 4 3 2
+1. 확인해야 하는 경우의 수가 g * g * ... 10^5 반복
+    - 시간초과
+2. 들어온 번호에 일단 채우기
+    - 시간초과
+3. g -> g-1의 루트라고 생각하기?
+    - union - find 방법 이용하기
 """
+import sys
+input = sys.stdin.readline
 
 def find(x):
-    while x != parent[x]:
-        x = parent[x]
+    if parent[x] != x:
+        parent[x] = find(parent[x])
     return parent[x]
 
 def union(a, b):
     x = find(a)
     y = find(b)
-    
-    if x == y:
-        return
+
+    if x > y:
+        parent[x] = y
+    else:
+        parent[y] = x
 
 G = int(input())
 P = int(input())
-g = []
-for _ in range(P):
-    g.append(int(input()))
 
 parent = [x for x in range(G+1)]
 
+plane = 0
+for _ in range(P):
+    gate = int(input())
+
+    boarding_number = find(gate)
+
+    if boarding_number == 0:
+        break
+
+    union(boarding_number, boarding_number-1)
+    plane += 1
+
+print(plane)
