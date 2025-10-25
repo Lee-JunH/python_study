e294bcf7e4d1d6cc068aeda375b843805959c43f
Lee-JunH <harry.lee1006@gmail.com>
2025-10-25 19:22:18 +0900

    트럭

--- Backjoon/13335_truck.py
+++ Backjoon/13335_truck.py
@@ -2,20 +2,34 @@
 Backjoon_13335 - 트럭 - S1
 
 문제
-- 
+- 하나의 차선으로 된 다리가 하나 있다. 이 다리를 n개의 트럭이 건러가려고 한다.
+- 다리 위는 w대의 트럭만 동시에 올라갈 수 있다. 다리의 길이는 w 단위길이이며,
+- 각 트럭들은 하나의 단위시간에 하나의 단위길이만큼 이동 가능하다.
+- 동시에 올라가는 트럭들의 무게 합은 다리의 최대하중인 L보다 작거나 같아야 한다.
+
+풀이
+- 다리에 올릴수 있는지 없는지로 나눠서 생각한다.
+- 1. 올라갈 수 있는 경우
+    - 트럭 무게확인 후 다리에 올리기
+- 2. 더이상 못올라가는 경우
+    - 다리 위에 있는 트럭 하나를 뺀다.
 """
 
 from collections import deque
 
-n, w, L = map(int, input())
-truck = list(map(int, input().split()))
+n, w, L = map(int, input().split())
+truck = deque(list(map(int, input().split())))
 
-my_t = deque()
-arrived = 0
-time = 0
-length = 0
-weight = 0
-index = 0
+bridge = deque([0] * w)    # 다리 위 정보
+time = 0    # 시간 저장
 
-while arrived < n:
-    
+while bridge:
+    time += 1
+    bridge.popleft()
+    if truck:
+        if sum(bridge) + truck[0] <= L:
+            new_truck = truck.popleft()
+            bridge.append(new_truck)
+        else:
+            bridge.append(0)        
+print(time)
