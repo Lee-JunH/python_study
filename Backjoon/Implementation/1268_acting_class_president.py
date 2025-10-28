fb92526e7f6fee2220c297d46f24a951a0fecea6
Kimkangyeon-17 <rkddus0517@naver.com>
2025-10-28 15:24:01 +0900

    임시 반장 정하기

--- Backjoon/Implementation/1268_acting_class_president.py
+++ Backjoon/Implementation/1268_acting_class_president.py
@@ -14,14 +14,14 @@ for grade in range(5):
     for a in range(N-1):  # a번 학생과
         for b in range(a+1, N):  # b번 학생 비교
             if my_class[a][grade] == my_class[b][grade]:    # a의 반 번호랑 b의 반 번호가 같으면
-                student[a].add(b)
-                student[b].add(a)
+                student[a].add(b)   # a의 친구 리스트에 b추가
+                student[b].add(a)   # b의 친구 리스트에 a추가
 
-many_friend_student = 0
-max_friend = -1
+many_friend_student = 0 # 친구가 많은 학생의 번호
+max_friend = -1         # 친구 수
 for i in range(N):
     cur_friend = len(student[i])
-    if cur_friend > max_friend:
+    if cur_friend > max_friend:     # 친구 수가 많으면
         max_friend = cur_friend
-        many_friend_student = i+1
+        many_friend_student = i   # 학생번호 업데이트
 print(many_friend_student)
