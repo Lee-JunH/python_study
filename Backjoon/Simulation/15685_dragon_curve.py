13044995ae480d3536dea7305d413583aca1d0b6
Lee-JunH <harry.lee1006@gmail.com>
2025-11-04 12:13:01 +0900

    드래곤 커브

--- Backjoon/Simulation/15685_dragon_curve.py
+++ Backjoon/Simulation/15685_dragon_curve.py
@@ -13,19 +13,46 @@ Backjoon_15685 - 드래곤 커브 - G3
 - 즉 K세대 드래곤 커브는 K-1세대 드래곤 커브를 끝 점을 기준으로 90도 시계방향 회전시킨 다음, 끝점에 붙인 것
 - 드래곤 커브 N개가 있을 때 네 꼭짓점이 모두 드래곤 커브의 일부인 정사각형의 개수는?
 - 0: 우, 1: 위, 2: 좌, 3: 아래
+
+풀이
+- 반복되는 방향들을 저장하고 방향에 따라 값 1로 바꾸고 사각형 개수 세기
 """
 
 def count_square():
-    pass
+    count = 0
+    for i in range(100):
+        for j in range(100):
+            if board[i][j] == 1 and board[i][j+1] == 1 and board[i+1][j] == 1 and board[i+1][j+1] == 1:
+                count += 1
+    return count
 
 def dragon_curve(gen):
-    for i in range(gen):
-        pass
+    curve = [d]
+    if gen >= 1:
+        curve.append((d+1) % 4)
+    for _ in range(1, gen):
+        cnt = len(curve)
+        for k in range(cnt): # 방향 반대로 바꿔주기
+            if k < cnt // 2:
+                curve.append((curve[k]+2) % 4)
+            else:
+                curve.append(curve[k])
+
+    r, c = y, x
+    for j in range(len(curve)):     # 채우기
+        r += dr[curve[j]]
+        c += dc[curve[j]]
+        board[r][c] = 1
 
 N = int(input())
+board = [[0 for _ in range(101)] for _ in range(101)]
+
+dr = [0, -1, 0, 1]
+dc = [1, 0, -1, 0]
+
 for _ in range(N):
     x, y, d, g = map(int, input().split())
-
+    board[y][x] = 1
     dragon_curve(g)
 
 print(count_square())
