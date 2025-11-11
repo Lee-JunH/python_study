d982d7a2c7f9d223a62b9c7a77c26e61bd14ade1
junh <harry.lee1006@gmail.com>
2025-11-12 02:49:33 +0900

    나무 재테크

--- Backjoon/16235_tree_Investment.py
+++ Backjoon/16235_tree_Investment.py
@@ -13,23 +13,65 @@ Backjoon_16235 - 나무 재테크 - G3
 - 가을에는 나무가 번식한다. 
     - 번식하는 나무는 나이가 5의 배수이어야 하며
     - 인접한 8개의 칸에 나이가 1인 나무가 생긴다.
-- 겨울에는 S2D2가 땅을 돌아다니면서 땅에 양분을 추가한다.
+- 겨울에는 로봇 S2D2가 땅을 돌아다니면서 땅에 양분을 추가한다.
     - 각 칸에 추가되는 양분의 양은 A[r][c]이다.
 - K년이 지난 후 상도의 땅에 살아있는 나무의 개수는?
 """
 
+def spring():
+    for i in range(N):
+        for j in range(N):
+            if trees[i][j]:
+                for k in range(len(trees[i][j])):
+                    if food[i][j] >= trees[i][j][k]:
+                        food[i][j] -= trees[i][j][k]
+                        trees[i][j][k] += 1
+                    else:
+                        for _ in range(k, len(trees[i][j])):
+                            food[i][j] += trees[i][j].pop() // 2
+                        break
+    return
+
+def summer():
+    pass
+
+def autumn():
+    for i in range(N):
+        for j in range(N):
+            for k in range(len(trees[i][j])):
+                if trees[i][j][k] % 5 == 0:
+                    for l in range(8):
+                        nx = i + dx[l]
+                        ny = j + dy[l]
+                        if 0 <= nx < N and 0 <= ny < N:
+                            trees[nx][ny].appendleft(1)
+            food[i][j] += dead[i][j]
+    return
+
+def winter():
+    pass
+
 N, M, K = map(int, input().split())     # N: 쿠기, M: 심는 나무, K: 지난 년 수
 A = [list(map(int, input().split())) for _ in range(N)]
-x, y, z = map(int, input().split()) 
+tree_temp = [list(map(int, input().split())) for _ in range(M)]
 
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 food = [[5 for _ in range(N)] for _ in range(N)]
+trees = [[[] for _ in range(N)] for _ in range(N)]
+dead = [[0 for _ in range(N)] for _ in range(N)]
+
+for x, y, z in tree_temp:        # 나무 정보 입력
+    trees[x-1][y-1].append(z)
 
 for _ in range(K):
-    # 봄
-    
-    # 여름
+    spring()
+    summer()
+    autumn()
+    winter()
 
-    # 가을
-
-    # 겨울
-    pass
+answer = 0
+for i in range(N):
+    for j in range(N):
+        answer += len(trees[i][j])
+print(answer)
