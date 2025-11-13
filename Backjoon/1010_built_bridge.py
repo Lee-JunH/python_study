717f7c26b69e6ad5c83f5c4343d9cd1f9f20d9c9
Lee-JunH <harry.lee1006@gmail.com>
2025-11-13 17:54:35 +0900

    다리 놓기

--- Backjoon/1010_built_bridge.py
+++ Backjoon/1010_built_bridge.py
@@ -12,4 +12,14 @@ T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
 
+    dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
+
+    for i in range(1, M+1):
+        dp[1][i] = i
     
+    # N이 2 이상일 때
+    for i in range(2, N+1):
+        for j in range(i, M+1):
+            for k in range(j, i-1, -1):
+                dp[i][j] += dp[i-1][k-1]
+    print(dp[N][M])
