68553484a4686e6392615ccc4cc0a77cbf29b23f
Lee-JunH <harry.lee1006@gmail.com>
2025-11-18 20:56:14 +0900

    전자카트

--- SWEA/5189_elec_cart.py
+++ SWEA/5189_elec_cart.py
@@ -3,29 +3,20 @@ SWEA_5189 - 전자카트 - D3
 
 문제
 - 사무실에서 출발해 각 구역을 한 번씩만 방문하고 사무실로 돌아올 때의 최소 배터리 사용량
-- 
 """
 
-from collections import deque
-
-def bfs():
+def dfs(cur, hap, cnt):
     global min_battery
 
-    my_q = deque()
-    my_q.append((start, 0))
-    
-    while my_q:
-        cur, hap = my_q.popleft()
-
-        if sum(vis) == N:
-            min_battery = min(min_battery, hap)
-        for i in range(N):
-            if i == cur:
-                continue
-            if vis[i] != 1:
-                vis[i] = 1
-                my_q.append((i, hap+battery[cur][i]))
-
+    if hap > min_battery:
+        return
+    if cnt == N:
+        min_battery = min(min_battery, hap+battery[cur][0])
+    for i in range(1, N):
+        if not vis[i]:
+            vis[i] = 1
+            dfs(i, hap+battery[cur][i], cnt + 1)
+            vis[i] = 0
 
 T = int(input())
 for case in range(T):
@@ -33,29 +24,9 @@ for case in range(T):
     battery = [list(map(int, input().split())) for _ in range(N)]
 
     vis = [0 for _ in range(N)]
-    vis[0] = 1
 
-    start = 0
     min_battery = float('inf')
 
-    bfs()
+    dfs(0, 0, 1)
 
     print(f'#{case+1} {min_battery}')
-
-
-# 3
-# 3
-# 0 18 34
-# 48 0 55
-# 18 7 0
-# 4
-# 0 83 65 97
-# 82 0 78 6
-# 19 19 0 82
-# 6 34 94 0
-# 5
-# 0 9 26 85 42
-# 14 0 84 31 27
-# 58 88 0 16 46
-# 83 61 94 0 17
-# 40 71 24 38 0
