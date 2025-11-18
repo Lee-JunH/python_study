4608a1f9701bd868b41b62236fa1c76e8c4ba81c
Lee-JunH <harry.lee1006@gmail.com>
2025-11-18 20:29:52 +0900

    최소합

--- SWEA/5188_min_sum.py
+++ SWEA/5188_min_sum.py
@@ -6,31 +6,21 @@ SWEA_5188 - 최소합 - D3
 - 맨 왼쪽 위에서 오른쪽 아래까지 이동할 때, 지나는 칸에 써진 숫자의 합계가 최소가 되는 합계
 """
 
-from collections import deque
-
-def bfs():
+def dfs(x, y, hap):
     global min_hap
 
-    my_q = deque()
-    my_q.append((0,0,board[0][0]))
-    dr = [0, 1]
-    dc = [1, 0]
-
-    while my_q:
-        r, c, hap = my_q.popleft()
-        if r == N-1 and c == N-1:
-            min_hap = min(min_hap, hap)
-            continue
-
-        for dir in range(2):
-            nr = r + dr[dir]
-            nc = c + dc[dir]
-            if nr < 0 or nr >= N or nc < 0 or nc >= N:
-                continue
-            if hap+board[nr][nc] > min_hap:
-                continue
-            my_q.append((nr, nc, hap+board[nr][nc]))
-
+    if x >= N or y >= N:
+        return
+    if hap > min_hap:
+        return
+    if x == N-1 and y == N-1:
+        min_hap = min(min_hap, hap)
+        return
+    
+    if x + 1 < N:
+        dfs(x+1, y, hap+board[x+1][y])
+    if y + 1 < N:
+        dfs(x, y+1, hap+board[x][y+1])
 
 T = int(input())
 for case in range(T):
@@ -38,25 +28,41 @@ for case in range(T):
     board = [list(map(int, input().split())) for _ in range(N)]
 
     min_hap = 261
-
-    bfs()
+    dfs(0,0,board[0][0])
     print(f'#{case+1} {min_hap}')
 
+# from collections import deque
+
+# def bfs():
+#     global min_hap
+
+#     my_q = deque()
+#     my_q.append((0,0,board[0][0]))
+#     dr = [0, 1]
+#     dc = [1, 0]
+
+#     while my_q:
+#         r, c, hap = my_q.popleft()
+#         if hap > min_hap:
+#             continue
+#         if r == N-1 and c == N-1:
+#             min_hap = min(min_hap, hap)
+#             continue
+
+#         for dir in range(2):
+#             nr = r + dr[dir]
+#             nc = c + dc[dir]
+#             if nr < 0 or nr >= N or nc < 0 or nc >= N:
+#                 continue
+#             my_q.append((nr, nc, hap+board[nr][nc]))
 
 
-# 3
-# 3
-# 1 2 3
-# 2 3 4
-# 3 4 5
-# 4
-# 2 4 1 3
-# 1 1 7 1
-# 9 1 7 10
-# 5 7 2 4
-# 5
-# 6 7 1 10 2
-# 10 2 7 5 9
-# 9 3 2 9 6
-# 1 6 8 2 9
-# 8 3 8 2 1
+# T = int(input())
+# for case in range(T):
+#     N = int(input())
+#     board = [list(map(int, input().split())) for _ in range(N)]
+
+#     min_hap = 261
+
+#     bfs()
+#     print(f'#{case+1} {min_hap}')
